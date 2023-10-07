a45ed10c5c93c6d86f3657c27a139e2e48cbb96c
mhmdnurf <mhmd.nurfat@gmail.com>
2023-10-08 03:38:22 +0700

    first commit

---
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticle()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticle() {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.article_title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.first_paragraph),
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            stringResource(R.string.second_paragraph),
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ComposeArticlePreview() {
+    ComposeArticleTheme {
+        ComposeArticle()
+    }
+}
